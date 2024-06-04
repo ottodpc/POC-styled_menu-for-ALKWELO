123494fc6d565ba8be223750eb5d7e713801dfc0
OTTO Dieu-Puissant Cyprien <cyprienotto@outlook.fr>
2024-06-04 16:55:48 +0200

    Working on the nav ui

---
+++ src/components/Nav/data.tsx
@@ -0,0 +1,41 @@
+export const links = [
+  {
+    title: "Hairstyle",
+    href: "/",
+  },
+  {
+    title: "Application",
+    href: "/",
+  },
+  {
+    title: "Customer",
+    href: "/",
+  },
+  {
+    title: "ALKWELO team",
+    href: "/",
+  },
+  {
+    title: "Contact",
+    href: "/",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Facebook",
+    href: "/",
+  },
+  {
+    title: "LinkedIn",
+    href: "/",
+  },
+  {
+    title: "Instagram",
+    href: "/",
+  },
+  {
+    title: "Twitter",
+    href: "/",
+  },
+];
